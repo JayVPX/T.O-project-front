4d3e27a09c67a10666d0b08876f6e76406d7ef2f
João Costa Praxedes <joaocostapraxedes@gmail.com>
2026-09-01 21:55:27 -0300

    chore: Initialize project

---
+++ src/App.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import heroImg from "./assets/hero.png";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "./assets/vite.svg";
+import "./App.css";
+// import axios from "axios";
+
+function App() {
+  const [count, setCount] = useState(0);
+  // const [appointments, setAppointments] = useState<unknown[]>([]);
+
+  // useEffect(() => {
+  //   async function render() {
+  //     const response = await axios.get(
+  //       "http://localhost:8000/api/appointments/get",
+  //     );
+
+  //     setAppointments(response.data);
+  //   }
+
+  //   render();
+  // }, []);
+
+  // console.log(appointments);
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={heroImg} className="base" width="170" height="179" alt="" />
+          <img src={reactLogo} className="framework" alt="React logo" />
+          <img src={viteLogo} className="vite" alt="Vite logo" />
+        </div>
+        <div>
+          <h1>Get started</h1>
+          <p>
+            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
+          </p>
+        </div>
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+    </>
+  );
+}
+
+export default App;
